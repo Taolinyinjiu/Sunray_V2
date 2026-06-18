666aaba2f6d31a7eca36b260b14d0d90bd07ba4b
Taolinyinjiu <qwer200413@163.com>
2026-06-18 11:49:48 +0800

    format drivers floder
    将SDK类的驱动依赖，转移到drivers/third_party_SDK文件夹中，一级drivers只留下ros相关驱动包

--- localization/localization_sources/sunray_viobot/src/viobot_odom_node.cpp
+++ localization/localization_sources/sunray_viobot/src/viobot_odom_node.cpp
@@ -1,3 +1,5 @@
+// TODO: 1. 添加VIOBOT算法启动命令？ 
+// 2. 添加参数用于快速切换高频与不同算法？
 #include <ros/ros.h>
 #include <nav_msgs/Odometry.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
@@ -16,6 +18,7 @@ void OdomCallback(const nav_msgs::Odometry::ConstPtr& msg) {
 
     nav_msgs::Odometry odom_msg = *msg;
 
+    // viobot默认里程计话题输出位置信息正确，姿态四元数与无人机不一致，需要转换，速度与角速度都需要变换
     tf2::Quaternion q;
     q.setW(msg->pose.pose.orientation.w);
     q.setX(msg->pose.pose.orientation.x);
@@ -34,13 +37,25 @@ void OdomCallback(const nav_msgs::Odometry::ConstPtr& msg) {
     q = q * q_z * q_y;
 
     // 发布里程计消息
+    // 位置一致，直接填充
     odom_msg.pose.pose.position.x = msg->pose.pose.position.x;
     odom_msg.pose.pose.position.y = msg->pose.pose.position.y;
     odom_msg.pose.pose.position.z = msg->pose.pose.position.z;
+    // 姿态不一致，填充变换后的姿态
     odom_msg.pose.pose.orientation.x = q.x();
     odom_msg.pose.pose.orientation.y = q.y();
     odom_msg.pose.pose.orientation.z = q.z();
     odom_msg.pose.pose.orientation.w = q.w();
+    // 速度方向不一致，转换速度方向
+    odom_msg.twist.twist.linear.x = -msg->twist.twist.linear.x;
+    odom_msg.twist.twist.linear.y =  msg->twist.twist.linear.y;
+    odom_msg.twist.twist.linear.z = -msg->twist.twist.linear.z;
+    // 角速度方向不一致，转换角速度
+    odom_msg.twist.twist.angular.x = -msg->twist.twist.angular.x;
+    odom_msg.twist.twist.angular.y =  msg->twist.twist.angular.y;
+    odom_msg.twist.twist.angular.z = -msg->twist.twist.angular.z;
+    
+    // 发布里程计数据
     viobot_odom_pub.publish(odom_msg);
 }
 
